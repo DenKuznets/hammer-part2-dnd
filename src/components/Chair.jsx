17776c4a212f64c4d3bc356e38936c34c40cea2c
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-06-28 13:09:23 +0300

    1 element is working

--- src/components/Chair.jsx
+++ src/components/Chair.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { useDrag } from "react-dnd";
+import { DragPreviewImage, useDrag } from "react-dnd";
 import styled from "styled-components";
 import { ItemTypes } from "../Constants";
+import dragPreview from "/images/chair3d.jpg";
 
 const ChairStyled = styled.div`
     /* width: 100px;
@@ -18,17 +19,27 @@ const ChairStyled = styled.div`
 `;
 
 const Chair = () => {
-    const [{ isDragging }, drag] = useDrag(() => ({
+    const [{ isDragging }, drag, preview] = useDrag(() => ({
         type: ItemTypes.CHAIR,
         collect: (monitor) => ({
             isDragging: !!monitor.isDragging(),
         }),
     }));
-
+    // console.log(preview);
     return (
-        <ChairStyled ref={drag} style={{ opacity: isDragging ? 0.5 : 1}}>
-            <img src="/images/chair.png" alt="" />
-        </ChairStyled>
+        <>
+            <DragPreviewImage
+                style={{
+                    width: "100px !important",
+                    height: "100px !important",
+                }}
+                connect={preview}
+                src={dragPreview}
+            />
+            <ChairStyled ref={drag} style={{ opacity: isDragging ? 0.5 : 1 }}>
+                <img src="/images/chair.png" alt="" />
+            </ChairStyled>
+        </>
     );
 };
 
